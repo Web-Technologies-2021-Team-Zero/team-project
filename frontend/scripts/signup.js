1d00466b81dee6fec1f5e6281deb8a859665f602
Lorraine Makuyana <58143262+lorrainemakuyana@users.noreply.github.com>
2021-11-17 00:56:01 +0000

    Add form validation

--- frontend/scripts/signup.js
+++ frontend/scripts/signup.js
@@ -12,24 +12,24 @@ let errors = 0;
 
 // show input error messages
 function showAllErrors(displayPlace, message) {
-    displayPlace.html(message);
+    displayPlace.innerHTML = message;
     errors += 1;
 }
 
 // Check for input lengths for register fields 
 function checkFieldLength(input, minLength, maxLength) {
     if (input.value.length <= minLength) {
-        showAllErrors(input.next(), `Your ${input.attr('id')} cannot be less than characters long`)
+        showAllErrors(input.nextElementSibling, `Your ${input.name} cannot be less than characters long`)
     }
     if (input.value.length >= maxLength) {
-        showAllErrors(input.next(), `Your ${input.attr('id')} cannot be more than characters long`)
+        showAllErrors(input.nextElementSibling, `Your ${input.name)} cannot be more than characters long`)
     }
 }
 
 function checkRequiredFields(inputs) {
     inputs.forEach(input => {
         if (input.value == '') {
-        showAllErrors(displayPlace, "This is a required field")
+        showAllErrors(input.nextElementSibling, "This is a required field")
     }
     })
     
@@ -38,29 +38,40 @@ function checkRequiredFields(inputs) {
 function checkUserEmail(input) {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     if (!input.value.match(emailRegex)) {
-        showAllErrors(input.next(), "This email is invalid")
+        showAllErrors(input.nextElementSibling, "This email is invalid")
+    }
+}
+
+function checkYeargroup(input) {
+    let y_group = parseInt(input.value)
+    if (y_group) {
+        if ((y_group < 2022) && (y_group > 2025) ) {
+            showAllErrors(input.nextElementSibling, "Your year group should be 2022, 2023, 2024 or 2025 only.")
+        }
     }
 }
 
 function checkMajor(input) {
     const allMajors = ['CE', 'EE', 'ME', 'ÇS', 'MIS', 'BA']; 
     if (!(allMajors.includes(input.value))) {
-        showAllErrors(input.next(), "Major not found. Please check above")
+        showAllErrors(input.nextElementSibling, "Major not found. Please check above")
     }
 }
 
-function confirmPassword(password, confirmPassword) {
-    if (password.value != confirmPassword.value) {
-        showAllErrors(confirmpassword.next(), "Your passwords must match tp continue")
+function confirmPassword(password1, password2) {
+    if (password1.value != password2.value) {
+        showAllErrors(input.nextElementSibling, "Your passwords must match to continue")
     }
 }
 
 function validateUserInputs(e) {
+    console.log("event caught")
     e.preventDefault() 
     errors = 0 
-    document.getElementsByTagName('small').forEach(tag => {
-        tag.innerHTML = ""
-    })
+    // document.getElementsByTagName('small').forEach(tag => {
+    //     tag.innerHTML = ""
+    // })
+    $('small').html('');
     checkFieldLength(fullname, 5, 50)
     checkFieldLength(password, 5, 15)
     checkRequiredFields([fullname, email, yeargroup, major, password, confirmpassword])
@@ -74,7 +85,7 @@ function validateUserInputs(e) {
     return false
 }
 
-document.getElementById("signup-button").addEventListener("click", () => {
-    let isCorrect = validateUserInputs()
-    if (isCorrect) location.href = "index.html"
-})
+// document.getElementById("signup-button").addEventListener("click", () => {
+//     let isCorrect = validateUserInputs()
+//     if (isCorrect) location.href = "index.html"
+// })
