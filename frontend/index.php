b7dd3082f308010b805b53a262680dfbc6ddf86d
Tracy <90452258+orpseal@users.noreply.github.com>
2021-11-20 11:16:23 +0000

    Changed html files to php

---
+++ frontend/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
+    <link href="./styles/main.css" rel="stylesheet" type="text/css" />
+
+    <title>Resource Hub | Team Zero</title>
+    <script src="./scripts/headings.js"></script>
+</head>
+<body onload="test()">
+    <nav>
+        <img src="./images/logo_v1.JPG" alt="Resource hub logo">
+            
+            <span>
+                <a href="#">Help</a>
+                <a href="login.html">Login</a>
+                <a href="signup.html">Sign Up</a>
+            </span>
+    </nav>
+    <div style="clear: both;"></div>
+    <main>
+        
+
+
+        
+    </main>
+
+
+    <script src="./scripts/index.js"></script>
+</body>
+</html>
