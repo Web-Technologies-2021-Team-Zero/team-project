f52151a77658e22f4e567d1a4df8b98ace8f04dc
Lorraine Makuyana <58143262+lorrainemakuyana@users.noreply.github.com>
2021-11-21 00:14:34 +0000

    Add responsiveness

--- frontend/index.php
+++ frontend/index.php
@@ -46,21 +46,22 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;1,100;1,300;1,400;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <link href="./styles/index.css" rel="stylesheet" type="text/css" />
+    <link href="./styles/main.css" rel="stylesheet" type="text/css" />
 
     <title>Resource Hub | Team Zero</title>
 </head>
 <body onload="">
     <nav>
-        <img src="./images/logo_v1.JPG" alt="Resource hub logo">
+        <a href="index.php"><img src="./images/logo_v1.JPG" alt="Resource hub logo"></a>
             
-            <span>
-                <a href="login.html">Login</a>
-                <a href="signup.html">Sign Up</a>
-            </span>
+        <span>
+            <a href="login.php">Login</a>
+            <a href="signup.php">Sign Up</a>
+        </span>
     </nav>
     <div style="clear: both;"></div>
     <main>
@@ -71,22 +72,22 @@
                   <div class="carousel-item active">
                     <img class="d-block w-100 carousel-img" src="./images/image2.jpg" alt="First slide">
                     <div class="carousel-caption heading">
-                        <h5 id="hello">Created Just For You, By you</h5>
+                        <h5 id="hello">Created by you, for you</h5>
                         <p id="get-resources">...get all your resources on the go...</p>
                     </div>
                   </div>
                   <div class="carousel-item">
                         <img class="d-block w-100 carousel-img" src="./images/image4.jpg" alt="Second slide">
                         <div class="carousel-caption heading">
-                            <h5 id="hello2">Want some other material aside class slides?</h5>
-                            <p id="get-resources2"><a href="signup.html">Register Now</a></p>
+                            <h5 id="hello2">All your resources in one place</h5>
+                            <p id="get-resources2"><a href="signup.php">Register Now</a></p>
                         </div>
                   </div>
                   <div class="carousel-item">
                         <img class="d-block w-100 carousel-img" src="./images/library1.jpg" alt="Third slide">
                         <div class="carousel-caption heading">
                             <h5 id="hello3">Oh, you already have an account?</h5>
-                            <p id="get-resources3"><a href="login.html">Log in now</a></p>
+                            <p id="get-resources3"><a href="login.php">Log in now</a></p>
                         </div>
                   </div>
                 </div>
@@ -103,18 +104,21 @@
         <section class="description">
             <div>
                 <h1>Ashesi Resource Hub</h1>
-                <h1>Your "one-stop-shop" for resources on-the-go</h1>
+                <h3><em>Your "one-stop-shop" for resources on-the-go</em></h3><br>
                 <p>
-                    So, you want to get some study material, not in this list->[lecture slides, textbooks, online material/links]? 
-                    <br>
-                    Look no further! 
-                    <br> 
+                    So, you want to get some study material, other than lecture slides, textbooks and online material/links?
+                    <br><br>
+                    <em style="color: #800000;">Look no further!</em>
+                    <br><br>
                     Here is the place where you will find any documents that have been shared by your peers and your seniors who have 
-                    passed through the same courses before you. How amazing is that? No calling or texting your seniors asking for past notes
-                    and any material you would need. Why? Because they would have uploaded all the material and you can find it 
-                    all in one place! Where? Here. So, can I ask what you are still waiting for? &#128579; 
-                    <br> 
-                    <button id="register-btn">Get an account today</button> 
+                    passed through the same courses before you.<br><br>
+                    How amazing is that? No calling or texting your seniors asking for past notes and any material you would need.<br><br> 
+                    <em style="color: #800000;">Why?</em> <br><br>
+                    Because they would have uploaded all the material and you can find it all in one place! <br><br>
+                    <em style="color: #800000;">Where?</em> <br><br>
+                    Here. <br><br>
+                    <em style="color: #800000;">So, can I ask what you are still waiting for?</em> &#128579; <br><br> 
+                    <button id="register-btn">Get Started</button>
                     <button id="signup-btn"> Log in </button>
                 </p>
             </div>
@@ -123,10 +127,11 @@
     <div class="footer-container">
         <footer>
             <div class="footer-content">
-                <img src="./images/logo_v1.JPG" alt="Resource Hub Logo">
-                <a href="about.html">About the Team</a>
-                <a href="login.html">Login to my account</a>
-                <a href="signup.html">Register for an account</a>
+                <a href="index.php"><img src="./images/logo_v1.JPG" alt="Resource Hub Logo"></a>
+            </div>
+            <div class="footer-links">
+                <a href="login.php">Login to my account</a>
+                <a href="signup.php">Register for an account</a>
                 <a href="https://github.com/Web-Technologies-2021-Team-Zero/team-project" target="_blank">Check Out Project Repository</a>
                 <span>&copy; 2021 Team Zero - CS341 Web Technologies</span>
             </div>
